9bd10acd665117b90f96a78a3ac61fb72a42e449
Jay Lee <jaylee.possible@gmail.com>
2020-07-20 03:51:18 +0900

    #7 creating new game page in progress

---
+++ src/scripts/enums/index.ts
@@ -0,0 +1,12 @@
+export const enum BlockColors {
+  ObstacleFill = "#31364A",
+  ObstacleStroke = "#000000",
+
+  InputFill = "#FFFFFF",
+  InputStroke = "#000000",
+}
+
+export const enum BlockTypes {
+  Obstacle,
+  Input,
+}
